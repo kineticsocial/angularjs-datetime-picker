973edc1e83aadeb8de4f4ed568ce38cffc18189d
Zack Townsend <ztownsend@praxsystech.com>
2016-03-09 14:49:12 -0500

    Add missing comma in package.json Fix two issues with IE11: (1) Calendar does not position correctly. Replace window.scrollX/scrollY with window.pageXOffset/pageYOffset to make it work in all browsers (2) Calendar can't be removed. Replace el.destroy() (not supported in pre-Edge IE) with el.parentElement.removeChild(el) to make it work in all browsers

--- angularjs-datetime-picker.js
+++ angularjs-datetime-picker.js
@@ -34,7 +34,7 @@
     var datetimePickerEl;
     var _this = this;
     var removeEl = function(el) {
-      el && el.remove();
+      el && el.parentElement.removeChild(el);
       $document[0].body.removeEventListener('click', _this.closeDatetimePicker);
     };
 
@@ -72,15 +72,15 @@
 
       datetimePickerEl.style.position='absolute';
       if(bcr.width > datePickerElBcr.width){
-        datetimePickerEl.style.left= (bcr.left + bcr.width - datePickerElBcr.width + window.scrollX) + 'px';
+        datetimePickerEl.style.left= (bcr.left + bcr.width - datePickerElBcr.width + window.pageXOffset) + 'px';
       } else {
-        datetimePickerEl.style.left= (bcr.left + window.scrollX) + 'px';
+        datetimePickerEl.style.left= (bcr.left + window.pageXOffset) + 'px';
       }
 
       if (bcr.top < 300 || window.innerHeight - bcr.bottom > 300) {
-        datetimePickerEl.style.top = (bcr.bottom + window.scrollY) + 'px';
+        datetimePickerEl.style.top = (bcr.bottom + window.pageYOffset) + 'px';
       } else {
-        datetimePickerEl.style.top = (bcr.top - datePickerElBcr.height + window.scrollY) + 'px';
+        datetimePickerEl.style.top = (bcr.top - datePickerElBcr.height + window.pageYOffset) + 'px';
       }
 
       $document[0].body.addEventListener('click', this.closeDatetimePicker);
